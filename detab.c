1caea174d0d46177b52bcf2639d7ecf089c244fa
JIeHb <gribak98@list.ru>
2017-03-31 21:09:55 +1000

    Create detab.c

---
+++ detab.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#define MAX_LEN 8
+#define SPACE " "
+
+int main() { 
+	int sym;
+	while((sym = getchar()) != EOF){
+		if(sym == '\t'){
+			printf(" {There tab, I found it, master}");
+			for(int i = 0; i < MAX_LEN; i++){
+				printf(SPACE);
+			}
+		}
+		else{
+			printf("%c", sym);
+		}
+	}
+return 0; 
