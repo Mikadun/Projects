5fc5f82ab13c28218e581b04dee7e5df0d30b09b
JIeHb <gribak98@list.ru>
2018-03-12 15:45:49 +1000

    Added print function

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 
+// For rand function in tests
+#include <stdlib.h>
+
 using namespace std;
 
 typedef int t_element;
@@ -30,7 +33,9 @@ t_element get_digit(t_element element, int pos){
 t_element* radix_sort(t_element *container, int size){
 	if(size < 2)
 		return container;
+    //cout << "We are going to create temporary array" << endl;
 	t_element temp_container[size];
+	//cout << "But u will not see this string, sadly" << endl;
 	int radix_count[radix];
 	int max_radix = get_max_radix(container, size);
 	for(int i = 0; i < max_radix; i++){
@@ -39,7 +44,7 @@ t_element* radix_sort(t_element *container, int size){
 		for(int j = 0; j < size; j++){
 			radix_count[get_digit(container[j], i)]++;
 		}
-		int temp, step = 0; 
+		int temp, step = 0;
 		for(int j = 0; j < radix; j++){
 			temp = radix_count[j];
 			radix_count[j] = step;
@@ -50,20 +55,29 @@ t_element* radix_sort(t_element *container, int size){
 			temp_container[radix_count[index]++] = container[j];
 		}
 		for(int j = 0; j < size; j++)
-			cout << temp_container[j] << " ";
-		cout << endl;
-		for(int j = 0; j < size; j++)
 			container[j] = temp_container[j];
 	}
 	return container;
-	
+
+}
+
+void print_container(t_element *container, int size){
+    for(int i = 0; i < size; i++){
+        cout << container[i] << " ";
+        if((i + 1) % 10 == 0)
+            cout << endl;
+    }
+    cout << endl;
 }
 
 int main(){
-	t_element arr[] = {312, 25, 1, 84, 6566, 656, 12, 7, 88, 901, 10000, 312};
-	cout << get_max_radix(arr, 12) << endl;
-	cout << get_digit(12345, 10) << endl;
-	radix_sort(arr, 12);
-	
+	const int test_size = 100;
+	const int test_digit_size = 1000;
+	t_element *arr = new t_element[test_size];
+	for(int i = 0; i < test_size; i++)
+		arr[i] = rand() % (test_digit_size - test_digit_size / radix) + test_digit_size / radix;
+    print_container(arr, test_size);
+	radix_sort(arr, test_size);
+	print_container(arr, test_size);
 	return 0;
 }
