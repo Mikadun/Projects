9dfbb106217a0b130a6e8b6df31e01ea4fa80e60
JIeHb <gribak98@list.ru>
2018-03-16 12:22:31 +1000

    Update list_to_matrix.py

--- list_to_matrix.py
+++ list_to_matrix.py
@@ -1,4 +1,9 @@
 n = int(input())
-for _ in range(n):
-	stream = input().split()
-	print(" ".join([str(int(str(i + 1) in stream)) for i in range(n)]))
+edges = [input().split() for _ in range(n)]
+graph = [['0' for _ in range(n)] for __ in range(n)]
+for i in range(n):
+	for j in edges[i]:
+	  if(j != '0'):
+	    graph[i][int(j) - 1] = '1'
+for i in graph:
+  print(" ".join(i))
