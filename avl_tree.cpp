a85272d36733416d284327d7ddd10714d8af8cde
JIeHb <gribak98@gmail.com>
2018-04-10 23:52:17 +1000

    Added t_tree class

--- avl_tree.cpp
+++ avl_tree.cpp
@@ -7,61 +7,124 @@ struct t_node
 	t_node(int _key) { key = _key; left = right = NULL; height = 1; }
 };
 
-// Main tree methods
-t_node* find_min(t_node *);
-t_node* insert(t_node *, int);
-t_node* search(t_node *, int);
-t_node* remove(t_node *, int);
+class t_tree 
+{
+	public:
+		t_tree() { _root = NULL; }
 
-t_node* balance(t_node *);
-t_node* rotate_right(t_node *);
-t_node* rotate_left(t_node *);
-int height(t_node *);
-int fix_height(t_node *);
-int bfactor(t_node *);
+		void print_tree(void);
+		void insert(int);
+		bool search(int);
+		void remove(int);
+	private:
+		t_node* _root;
+		t_node* find_min(t_node *);
+		t_node* insert_node(t_node *, int);
+		t_node* search_node(t_node *, int);
+		t_node* remove_node(t_node *, int);
 
-// Print tree functions
-void print(t_node *);
-void print_tree(t_node *, int);
+		t_node* balance(t_node *);
+		t_node* rotate_right(t_node *);
+		t_node* rotate_left(t_node *);
+		int height(t_node *);
+		int fix_height(t_node *);
+		int bfactor(t_node *);
+		void print_node(t_node *);
+};
 
 int main()
 {
-	t_node *root = NULL;
-	root = insert(root, 10);
-	root = insert(root, 20);
-	root = insert(root, 30);
-	root = insert(root, 32);
-	root = insert(root, 25);
-	root = insert(root, 15);
-	root = insert(root, 16);
-	root = insert(root, 12);
-	root = insert(root, 5);
-	root = insert(root, 8);
-	root = remove(root, 25);
-	print_tree(root, 0);
+	t_tree tree;
+	tree.insert(5);
+	tree.insert(1);
+	tree.insert(4);
+	tree.insert(-1);
+	tree.remove(5);
+	tree.print_tree();
 }
 
-void print(t_node *node)
+void t_tree::print_tree(void)
+{
+	print_node(_root);
+}
+
+void t_tree::insert(int key)
+{
+	_root = insert_node(_root, key);
+}
+
+bool t_tree::search(int key)
+{
+	return search_node(_root, key) == NULL;
+}
+
+void t_tree::remove(int key)
+{
+	_root = remove_node(_root, key);
+}
+
+void t_tree::print_node(t_node *node)
 {
 	if(node)
 	{
-		print(node->left);
-		std::cout << node->height << ": " << node->key << std::endl;
-		print(node->right);
+		print_node(node->left);
+		std::cout << node->key << " ";
+		print_node(node->right);
 	}
 }
 
-void print_tree(t_node *node, int depth)
+t_node* t_tree::insert_node(t_node *node, int key)
 {
-	if(node)
-	{
-		std::cout << depth << ": " << node->key << std::endl;
-		print_tree(node->left, depth + 1);
-		print_tree(node->right, depth + 1);
-	}
+	if(node == NULL) return new t_node(key);
+	if(key < node->key)
+		node->left = insert_node(node->left, key);
+	else
+		node->right = insert_node(node->right, key);
+	return balance(node);
 }
 
-int height(t_node *node)
+t_node* t_tree::search_node(t_node *node, int key)
+{
+	if(node == NULL || node->key == key) return node;
+	if(key < node->key)
+		return search_node(node->left, key);
+	else
+		return search_node(node->right, key);
+}
+
+t_node* t_tree::remove_node(t_node *node, int key)
+{
+	if(node == NULL) return node;
+	if(key < node->key)
+		node->left = remove_node(node->left, key);
+	else if(key > node->key)
+		node->right = remove_node(node->right, key);
+	else
+	{
+		if(node->left == NULL || node->right == NULL)
+		{
+			t_node *temp = node->left ? node->left : node->right;
+			if(temp == NULL)
+			{
+				temp = node;
+				node = NULL;
+			}
+			else
+				*node = *temp;
+			delete temp;
+		}
+		else
+		{
+			t_node *temp = find_min(node->right);
+			node->key = temp->key;
+			node->right = remove_node(node->right, node->key);
+		}
+	}
+	return balance(node);
+}
+
+
+int t_tree::height(t_node *node)
 {
 	if(node == NULL)
 		return 0;
@@ -69,19 +132,19 @@ int height(t_node *node)
 		return node->height;
 }
 
-int fix_height(t_node *node)
+int t_tree::fix_height(t_node *node)
 {
 	int hl = height(node->left);
 	int hr = height(node->right);
 	node->height = (hl > hr ? hl : hr) + 1;
 }
 
-int bfactor(t_node *node)
+int t_tree::bfactor(t_node *node)
 {
 	return height(node->right) - height(node->left);
 }
 
-t_node* rotate_right(t_node *node)
+t_node* t_tree::rotate_right(t_node *node)
 {
 	t_node* temp = node->left;
 	node->left = temp->right;
@@ -91,7 +154,7 @@ t_node* rotate_right(t_node *node)
 	return temp;
 }
 
-t_node* rotate_left(t_node *node)
+t_node* t_tree::rotate_left(t_node *node)
 {
 	t_node* temp = node->right;
 	node->right = temp->left;
@@ -101,7 +164,7 @@ t_node* rotate_left(t_node *node)
 	return temp;
 }
 
-t_node* balance(t_node *node)
+t_node* t_tree::balance(t_node *node)
 {
 	if(!node)
 		return node;
@@ -121,59 +184,9 @@ t_node* balance(t_node *node)
 	return node;
 }
 
-t_node* find_min(t_node *node)
+t_node* t_tree::find_min(t_node *node)
 {
 	if(node->left)
 		return find_min(node->left);
 	return node;
-}
-
-t_node* insert(t_node *node, int key)
-{
-	if(node == NULL) return new t_node(key);
-	if(key < node->key)
-		node->left = insert(node->left, key);
-	else
-		node->right = insert(node->right, key);
-	return balance(node);
-}
-
-t_node* search(t_node *node, int key)
-{
-	if(node == NULL || node->key == key) return node;
-	if(key < node->key)
-		return search(node->left, key);
-	else
-		return search(node->right, key);
-}
-
-t_node* remove(t_node *node, int key)
-{
-	if(node == NULL) return node;
-	if(key < node->key)
-		node->left = remove(node->left, key);
-	else if(key > node->key)
-		node->right = remove(node->right, key);
-	else
-	{
-		if(node->left == NULL || node->right == NULL)
-		{
-			t_node *temp = node->left ? node->left : node->right;
-			if(temp == NULL)
-			{
-				temp = node;
-				node = NULL;
-			}
-			else
-				*node = *temp;
-			delete temp;
-		}
-		else
-		{
-			t_node *temp = find_min(node->right);
-			node->key = temp->key;
-			node->right = remove(node->right, node->key);
-		}
-	}
-	return balance(node);
 }
