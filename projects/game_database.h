7a92893e56bad20705454a61ce8c1611dcf497bf
JIeHb <gribak98@list.ru>
2018-11-24 13:58:51 +1000

    Update game_database.h

--- projects/game_database.h
+++ projects/game_database.h
@@ -1,8 +1,7 @@
 // Copyright 2018 Gribak Jaroslav
 
-#include <set>
 #include <map>
-#include <unordered_map>
+#include <set>
 #include <string>
 #include <iterator>
 #include <utility>
@@ -11,13 +10,11 @@
 #include <algorithm>
 
 using std::string;
-using std::set;
 using std::pair;
 using std::make_pair;
 using std::vector;
 using std::map;
-using std::unordered_map;
-
+using std::set;
 
 bool Compare(const GameObject& a, const GameObject& b) {
     return a.id > b.id;
@@ -27,12 +24,19 @@ bool operator>(const GameObject& a, const GameObject& b) {
   return a.id > b.id;
 }
 
-bool operator<(const GameObject& a, const GameObject& b) {
-  return a.id < b.id;
-}
+template<class T, template<class> class Compare>
+class DereferenceCompare {
+ public:
+    bool operator()(const T* const a, const T* const b) {
+      return comp(*a, *b);
+    }
+ private:
+    Compare<T> comp;
+};
 
 using TPos = pair<size_t, size_t>;
 using TVec = vector<GameObject>;
+using TSet = set<GameObject*, DereferenceCompare<GameObject, std::greater>>;
 
 class GameDatabase {
  public:
@@ -46,20 +50,30 @@ class GameDatabase {
     vector<GameObject> Data() const;
 
  private:
-    map<ObjectId, GameObject> data;
+    map<ObjectId, GameObject, std::greater<ObjectId>> data;
     // map<TPos, TVec> by_pos;
-    map<string, TVec> by_name;
+    map<string, TSet> by_name;
 };
 
 void GameDatabase::Insert(ObjectId id, string name, size_t x, size_t y) {
   GameObject temp = {id, name, x, y};
-  data.insert_or_assign(id, temp);
-  by_name[name].push_back(temp);
-  // by_pos[TPos(x, y)].push_back(temp);
+  if (data.find(id) != data.end())
+    Remove(id);
+  data.insert(make_pair(id, temp));
+  by_name[name].insert(&data[id]);
+  /*
+  TPos pos = make_pair(x, y);
+  by_pos[pos].push_back(temp);
+  */
 }
 
 void GameDatabase::Remove(ObjectId id) {
-  data.erase(id);
+  auto it = data.find(id);
+  if (it != data.end()) {
+    GameObject obj = it->second;
+    by_name[obj.name].erase(&obj);
+    data.erase(it);
+  }
 }
 
 pair<GameObject, bool> GameDatabase::DataById(ObjectId id) const {
@@ -69,22 +83,21 @@ pair<GameObject, bool> GameDatabase::DataById(ObjectId id) const {
 
 vector<GameObject> GameDatabase::DataByName(string name) const {
   vector<GameObject> result;
-  TVec container = by_name.find(name)->second;
-  for (auto it = container.begin(); it != container.end(); it++) {
-    auto in_data = data.find(it->id);
-    if (in_data != data.end() && in_data->second.name == name) {
-      result.push_back(in_data->second);
+  if (by_name.find(name) != by_name.end()) {
+    TSet container = by_name.find(name)->second;
+    for (auto it = container.begin(); it != container.end(); it++) {
+      result.push_back(**it);
     }
   }
-
-  std::sort(result.begin(), result.end(), Compare);
   return result;
 }
 
 /*
 vector<GameObject> GameDatabase::DataByPosition(size_t x, size_t y) const {
   vector<GameObject> result;
-  TPos pos(x, y);
+  TPos pos = make_pair(x, y);
+  if (by_pos.find(pos) == by_pos.end())
+    return result;
   TVec container = by_pos.find(pos)->second;
   for (auto it = container.begin(); it != container.end(); it++) {
     auto in_data = data.find(it->id);
@@ -114,6 +127,5 @@ vector<GameObject> GameDatabase::Data() const {
   vector<GameObject> result;
   for (auto it = data.begin(); it != data.end(); it++)
     result.push_back(it->second);
-  std::sort(result.begin(), result.end(), Compare);
   return result;
 }
