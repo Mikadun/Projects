20ed184e6219c0203d977a27ec199c1ae5ac79f0
JIeHb <gribak98@list.ru>
2018-12-10 14:43:06 +1000

    Create line_intersection.py

---
+++ projects/line_intersection.py
@@ -0,0 +1,14 @@
+n = int(input())
+points = []
+p = sorted([sorted([int(j) for j in input().split()]) for _ in range(n)])
+for i in range(n):
+  points.append({'l': p[i][0], 'r': p[i][1], 'idx': i + 1})
+
+result = set()
+for i in range(n - 1):
+  for j in range(i + 1, n):
+    if points[i]['l'] <= points[j]['l'] <= points[i]['r']:
+      result.add(tuple(sorted([points[i]['idx'], points[j]['idx']])))
+
+for i in result:
+  print(i)
