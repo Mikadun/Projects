45a6b594f3dcafe29c8f736b8c1dca3c2b96b660
JIeHb <gribak98@list.ru>
2020-03-20 13:36:37 +1000

    Update RSA.py

--- projects/RSA.py
+++ projects/RSA.py
@@ -1,8 +1,10 @@
 from math import ceil, sqrt
 from random import randint
+import matplotlib.pyplot as plt
+from timeit import timeit
+import time
 
 def power(n, p, mod):
-    # Rapid exponentiation
     if p == 1:
         return n
     n %= mod
@@ -17,7 +19,6 @@ def power(n, p, mod):
     return t
 
 def trial_division(n):
-    # Check if n is prime
     if n < 2:
         raise Exception('Value error: number must be greater than 1')
     if n % 2 == 0:
@@ -29,29 +30,35 @@ def trial_division(n):
     return True
 
 def Miller_iteration(d, n):
-    a = randint(2, n - 2)
+    a = randint(3, n - 2)
     a_exp_d = power(a, d, n)
     
-    # First condition
     if a_exp_d == 1 or a_exp_d == n - 1:
         return True
     
-    while not d == n - 1:
-        a_exp_d = a_exp_d ** 2
+    while d != n - 1:
+        a_exp_d = (a_exp_d ** 2) % n
         d *= 2
+        if a_exp_d == 1:
+            return False
+
         if a_exp_d == n - 1:
             return True
     return False
 
 def Miller_Rabin(n, checks = 30):
-    if n == 3:
+    if n <= 1:
+        return False
+
+    if n <= 3:
         return True
+    
     if n % 2 == 0:
         return False
 
     d = n - 1
-    while not d & 1:
-        d = d >> 1
+    while d % 2 == 0:
+        d //= 2
 
     for check in range(checks):
         if not Miller_iteration(d, n):
@@ -73,54 +80,115 @@ def extended_gcd(a, b):
     y = x1
     return res, x, y
 
-class RSA:
-    def __init__(self, bit_length):
-        self.is_prime = Miller_Rabin
-        p, q = self.generate_prime(bit_length), self.generate_prime(bit_length)
-        fi = (p - 1) * (q - 1)
-        self.key_public = self.generate_public(fi)
-        self.key_private = self.generate_secret(self.key_public, fi)
-        self.module = p * q
+def Jacobi(n, m):
+    s = 1
+    if gcd(n, m) != 1:
+        return 0
 
-    def generate_prime(self, bit_length):
-        num = randint(2 ** (bit_length - 1), 2 ** bit_length)
-        while not self.is_prime(num):
-            num = randint(2 ** (bit_length - 1), 2 ** bit_length)
-        return num
-
-    def generate_public(self, fi):
-        max_length = len(str(fi)) - 2
-        while True:
-            length = randint(max_length // 2, max_length)
-            one_bit_amount = length // 10
-            num = (1 << length) + 1
-            for i in range(one_bit_amount):
-                num |= (1 << randint(1, length - 1))
-                if gcd(num, fi) == 1:
-                    return num
+    while True:
+        n = n % m
+        k = 0
+        while n % 2 == 0:
+            n = n >> 1
+            k += 1
+        k %= 2
         
+        if k == 1 and (m % 8 == 3 or m % 8 == 5):
+            s *= -1
+        
+        if n == 1 or (n == m - 1 and m % 4 == 1):
+            return s
 
-    def generate_secret(self, public, fi):
-        d = extended_gcd(self.key_public, fi)[1]
-        if d < 0:
-            d += fi
-        return d
+        if n == m - 1 and m % 4 == 3:
+            return -s
 
-    def encode(self, message, key = None):
-        result = ''
-        for c in message:
-            result += str(power(ord(c), self.key_public, self.module)) + ' '
+        n, m = m, n
+        if (n - 1) % 4 and (m - 1) % 4:
+            s *= -1
 
-        return result
+def Solovay_Strassen(n, checks = 30):
+    if n % 2 == 0:
+        return False
+
+    if n % 3 == 0:
+        return False
+
+    for _ in range(checks):
+        a = randint(3, n - 1)
+        if power(a, (n - 1) // 2, n) != (Jacobi(a, n) % n):
+            return False
+    return True
+
+
+def rsa(bit_length, is_prime = Miller_Rabin):
+    p, q = generate_prime(bit_length, is_prime), generate_prime(bit_length, is_prime)
+    fi = (p - 1) * (q - 1)
+    e = generate_public(fi)
+    d = generate_secret(e, fi)
+    n = p * q
+    return (e, n), (d, n)
+
+def generate_prime(bit_length, is_prime):
+    num = randint(2 ** (bit_length - 1) + 1, 2 ** bit_length)
+    while not is_prime(num):
+        num = randint(2 ** (bit_length - 1), 2 ** bit_length)
+    return num
+
+def generate_public(fi):
+    max_length = len(str(fi)) - 2
+    while True:
+        length = randint(max_length // 2, max_length)
+        if length < 3:
+            length = max_length
+        one_bit_amount = length // 10
+        if one_bit_amount < 3:
+            one_bit_amount = 2
+
+        num = (1 << length) + 1
+        for i in range(one_bit_amount):
+            num |= (1 << randint(1, length - 1))
+            if gcd(num, fi) == 1:
+                return num
     
-    def decode(self, message):
-        result = ''
-        for c in message.split():
-            result += chr(power(int(c), self.key_private, self.module))
 
-        return result
+def generate_secret(e, fi):
+    d = extended_gcd(e, fi)[1]
+    if d < 0:
+        d += fi
+    return d
 
-rsa = RSA(128)
-code = rsa.encode('Hello, world!')
-decode = rsa.decode(code)
-print(code, decode)
+def encode(message, public_key):
+    result = ''
+    for c in message:
+        result += str(power(ord(c), public_key[0], public_key[1])) + ' '
+
+    return result
+
+def decode(message, private_key):
+    result = ''
+    for c in message.split():
+        result += chr(power(int(c), private_key[0], private_key[1]))
+
+    return result
+
+def lineplot(ax, x_data, y_data, x_label="", y_label="", color="#539caf"):
+    ax.plot(x_data, y_data, lw = 2, color=color, alpha = 1)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+_range = range(5, 11)
+_, ax = plt.subplots()
+ax.set_title('RSA: comparasion of prime test algorithms')
+
+def test(_range, ax, algorithm, color='blue'):
+    times = []
+
+    for i in _range:
+        t = time.time()
+        rsa(2 ** i, algorithm)
+        times.append(time.time() - t)
+    lineplot(ax, _range, times, y_label = 'seconds', x_label = 'length', color=color)
+
+test(_range, ax, Miller_Rabin, 'red')
+test(_range, ax, Solovay_Strassen, 'blue')
+plt.show()
